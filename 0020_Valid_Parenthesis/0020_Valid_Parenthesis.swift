ca6834ba31b9f5628fa6e2b47e1add022496c069
JinLee <jinlee3511@gmail.com>
2024-04-10 01:19:37 -0700

    complete valid-parenthesis swift

--- 0020_Valid_Parenthesis/0020_Valid_Parenthesis.swift
+++ 0020_Valid_Parenthesis/0020_Valid_Parenthesis.swift
@@ -1,5 +1,28 @@
 class Solution {
     func isValid(_ s: String) -> Bool {
-        
+        var stack: [Character] = []
+        let opp: [Character: Character] = ["]":"[", "}":"{", ")":"("]
+
+        for ch: Character in s {
+            // Found Closing
+            if let oppCh: Character = opp[ch] {
+                guard let lastSeen = stack.popLast() else { 
+                    // closing came first than opening -> Invalid
+                    return false 
+                }
+
+                // Unmatch -> Invalid
+                if lastSeen != opp[ch] {
+                    return false
+                }
+            }
+
+            // Found Opening
+            else {
+                stack.append(ch)
+            }
+        }
+
+        return stack.count == 0
     }
 }
