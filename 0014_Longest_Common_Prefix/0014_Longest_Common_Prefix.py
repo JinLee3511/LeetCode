6ae008c06223664e08b8b7b881def50bdf32d25e
JinLee <jinlee3511@gmail.com>
2024-04-23 11:28:01 -0700

    update 14

--- 0014_Longest_Common_Prefix/0014_Longest_Common_Prefix.py
+++ 0014_Longest_Common_Prefix/0014_Longest_Common_Prefix.py
@@ -4,14 +4,17 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         prefix = strs[0]
 
-        for word in strs:
+        for i, word in enumerate(strs):
+            if i == 0:
+                continue
+
             new_prefix = ""
-            for ch_i, ch_j in zip(word, prefix):
+            for ch_i, ch_j in zip(prefix, word):
                 if ch_i == ch_j:
                     new_prefix += ch_i
                 else:
                     break
-            
+
             prefix = new_prefix
-            
+                
         return prefix
